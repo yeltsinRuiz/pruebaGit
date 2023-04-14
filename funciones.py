02f84f5142d7f87797f009ff3f77c80f236f422a
Yeltsin Ruiz <yeltsin335@gmail.com>
2023-04-13 21:10:44 -0500

    inicio calculadora

---
+++ funciones.py
@@ -0,0 +1,5 @@
+def suma(numero1,numero2):
+      return numero1+numero2
+
+def resta(numero1,numero2):
+      return numero1-numero2
---
+++ main.py
@@ -0,0 +1,3 @@
+from funciones import suma,resta
+
+print(suma(2,3))
